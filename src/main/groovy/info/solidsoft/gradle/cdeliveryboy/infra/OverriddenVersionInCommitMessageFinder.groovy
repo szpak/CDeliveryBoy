d82262c53763f462f29e90e17a68588812a06682
Marcin Zajączkowski <szpak@users.sourceforge.net>
2017-09-15 13:28:22 +0200

    Simplify regexp

--- src/main/groovy/info/solidsoft/gradle/cdeliveryboy/infra/OverriddenVersionInCommitMessageFinder.groovy
+++ src/main/groovy/info/solidsoft/gradle/cdeliveryboy/infra/OverriddenVersionInCommitMessageFinder.groovy
@@ -14,8 +14,8 @@ class OverriddenVersionInCommitMessageFinder {
 
     //https://github.com/mojombo/semver/issues/232
     private static final Pattern SEM_VER_PATTERN = Pattern.compile("\\[#((0|[1-9]\\d*)\\.(0|[1-9]\\d*)\\.(0|[1-9]\\d*)" +
-            "(-(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(\\.(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\\+[0-9a-zA-Z-]+(\\.[0-9a-zA-Z-]+)*)?)\\]")
-    private static final Pattern INCREMENTER_PATTERN = Pattern.compile("\\[#(MAJOR|MINOR|PATCH|PRERELEASE)\\]")
+            "(-(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*)(\\.(0|[1-9]\\d*|\\d*[a-zA-Z-][0-9a-zA-Z-]*))*)?(\\+[0-9a-zA-Z-]+(\\.[0-9a-zA-Z-]+)*)?)]")
+    private static final Pattern INCREMENTER_PATTERN = Pattern.compile("\\[#(MAJOR|MINOR|PATCH|PRERELEASE)]")
 
     private static final List<Pattern> SUPPORTED_PATTERNS = [SEM_VER_PATTERN, INCREMENTER_PATTERN]
 
