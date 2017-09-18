94d477ce61e009fb65c8e2ed09f953e961a58890
Marcin Zajączkowski <szpak@users.sourceforge.net>
2017-09-18 18:11:40 +0200

    [#12] Simplify testing with different Gradle versions
    More duplication, but much more readable.

--- src/test/groovy/info/solidsoft/gradle/cdeliveryboy/NotInReleaseModeFuncSpec.groovy
+++ src/test/groovy/info/solidsoft/gradle/cdeliveryboy/NotInReleaseModeFuncSpec.groovy
@@ -1,66 +1,40 @@
 package info.solidsoft.gradle.cdeliveryboy
 
-import groovy.transform.Canonical
 import info.solidsoft.gradle.cdeliveryboy.fixture.WithGradleVersionsDeterminer
 import org.gradle.testkit.runner.BuildResult
 import org.junit.Rule
 import org.junit.contrib.java.lang.system.EnvironmentVariables
-import spock.lang.Shared
 
 class NotInReleaseModeFuncSpec extends BaseTestKitFuncSpec implements WithProjectInExternalGitRepo, WithGradleVersionsDeterminer {
 
     @Rule
     public final EnvironmentVariables environmentVariables = new EnvironmentVariables();
 
-    def "finish build successfully and display missed conditions on '#ciTask' performed in non release mode"() {
-        given:
-            prepareNonReleasingTravisEnvironmentVariables()
-        when:
-            BuildResult result = runTasks(ciTask)
-        then:
-            result.output.contains("✘ IN RELEASE MODE")
-            result.tasks.collect { it.path }.containsAll(expectedTasksToExecute)
-        where:
-            ciTask              || expectedTasksToExecute
-            "prepareForCiBuild" || []
-            "ciBuild"           || [":test", ":build"]
-    }
-
-    @Shared
-    TaskAndVersion tasksAndVersions = generateCiTasksAndGradleVersionsCombinations()
-
-    def "finish build successfully and display missed conditions on '#ciTask' performed in non release mode with Gradle '#gradleVersionToTest'"() {
+    def "finish build successfully and display missed conditions on 'prepareForCiBuild' performed in non release mode with Gradle '#gradleVersionToTest'"() {
         given:
             prepareNonReleasingTravisEnvironmentVariables()
         and:
             gradleVersion = gradleVersionToTest
         when:
-            BuildResult result = runTasks(ciTask)
+            BuildResult result = runTasks("prepareForCiBuild")
         then:
             result.output.contains("✘ IN RELEASE MODE")
         where:
-            ciTask << tasksAndVersions.taskNames
-            gradleVersionToTest << tasksAndVersions.versions
+            gradleVersionToTest << determineGradleVersionsToTest()
     }
 
-    private TaskAndVersion generateCiTasksAndGradleVersionsCombinations() {
-        List<String> expectedTasksToExecute = ["prepareForCiBuild", "ciBuild"]
-        List<String> versionsToExecute = determineGradleVersionsToTest()
-
-        List<List<String>> combinations = GroovyCollections.combinations([expectedTasksToExecute, versionsToExecute] as Iterable)
-
-        TaskAndVersion tasksAndVersions = new TaskAndVersion(combinations*.get(0), combinations*.get(1))
-
-        assert tasksAndVersions.taskNames.containsAll(expectedTasksToExecute)
-        assert tasksAndVersions.versions.containsAll(versionsToExecute)
-
-        return tasksAndVersions
-    }
-
-    @Canonical
-    private static class TaskAndVersion {
-        List<String> taskNames
-        List<String> versions
+    def "finish build successfully and display missed conditions on 'ciBuild' performed in non release mode with Gradle '#gradleVersionToTest'"() {
+        given:
+            prepareNonReleasingTravisEnvironmentVariables()
+        and:
+            gradleVersion = gradleVersionToTest
+        when:
+            BuildResult result = runTasks("ciBuild")
+        then:
+            result.output.contains("✘ IN RELEASE MODE")
+            result.tasks.collect { it.path }.containsAll([":test", ":build"])
+        where:
+            gradleVersionToTest << determineGradleVersionsToTest()
     }
 
     def "override configured plugin configuration from command line"() {
